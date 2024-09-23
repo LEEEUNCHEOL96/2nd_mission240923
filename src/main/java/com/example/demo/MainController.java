d342321899899d17a5e6ff9e9b1e536f5034cf3d
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-23 09:08:07 +0900

    setting

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,8 +1,14 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
-
+    @GetMapping("/article")
+    @ResponseBody
+    public String article(){
+        return "article 입니다.";
+    }
 }
