76bce6c8e543cfc01b679abc69983441d09b20eb
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-23 11:07:43 +0900

    user entity,repo 생성

---
+++ src/main/java/com/example/demo/User/SiteUser.java
@@ -0,0 +1,23 @@
+package com.example.demo.User;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+}
