2b2bd4dafd4c41d678ae28c949be08a933ffac78
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-23 10:50:30 +0900

    paging 역순처리완료

--- src/main/java/com/example/demo/Article/ArticleService.java
+++ src/main/java/com/example/demo/Article/ArticleService.java
@@ -4,9 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -37,8 +39,11 @@ public class ArticleService {
         this.articleRepository.save(article);
     }
 
-    public Page<Article> getList(int page){
-        Pageable pageable = PageRequest.of(page,10);
+    public Page<Article> getList(int page) {
+        List<Sort.Order> sorts = new ArrayList<>();
+        sorts.add(Sort.Order.desc("createDate"));
+
+        Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
         return this.articleRepository.findAll(pageable);
     }
 }
