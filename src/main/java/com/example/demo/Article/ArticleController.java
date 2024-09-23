48a72d5b6b8197a19e59bc3e1969aeefd3b48fbe
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-09-23 09:30:21 +0900

    article,articleRepository 생성

--- src/main/java/com/example/demo/Article/ArticleController.java
+++ src/main/java/com/example/demo/Article/ArticleController.java
@@ -1,14 +1,25 @@
 package com.example.demo.Article;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/article")
+@RequiredArgsConstructor
 public class ArticleController {
+
+    private final ArticleRepository articleRepository;
+
     @GetMapping("/list")
-    public String articleList(){
+    public String articleList(Model model){
+        List<Article> articleList = this.articleRepository.findAll();
+        model.addAttribute("article",articleList);
+
         return "article_list";
     }
 }
